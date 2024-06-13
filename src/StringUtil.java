b6a4f5577d34b23c43ae5f6ff29ce0b3b1252055
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2024-06-13 09:43:37 +0530

    Added Starter Code

---
+++ src/StringUtil.java
@@ -0,0 +1,12 @@
+import java.nio.charset.StandardCharsets;
+
+class StringUtil {
+    // Apply Sha256 to a string and return the result.
+    public static String applySha256(String input) {
+
+
+    // Returns difficulty string target, to compare to hash. e.g. difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+
+    }
+}
